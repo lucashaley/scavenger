81a2d0db2dbd1de22c5f922be0672118d006a19d
Lucas Haley <lucashaley@users.noreply.github.com>
2024-06-25 18:43:23 -0700

    Door buffers

--- app/door.rb
+++ app/door.rb
@@ -24,19 +24,19 @@ class Door < Zif::Sprite
     @sound_bounce = "sounds/clank.wav"
 
     @door_side = door_side
-    @door_buffer = 4
+    @door_tolerance = 8
   end
 
   def collide_action collidee, facing
     # puts "Door collision: #{facing} vs #{@door_side}"
     if facing == @door_side
-      # puts "Player center_y: #{collidee.center_y}"
-      # puts "Door center_y: #{center_y}"
+      puts "Player center_y: #{collidee.center_y}"
+      puts "Door center_y: #{center_y}"
       entering = case @door_side
       when Faceable::FACING::north, Faceable::FACING::south
-        collidee.center_x.between?(center_x - @door_buffer, center_x + @door_buffer)
+        collidee.center_x.between?(center_x - @door_tolerance, center_x + @door_tolerance)
       when Faceable::FACING::east, Faceable::FACING::west
-        collidee.center_y.between?(center_y - @door_buffer, center_y + @door_buffer)
+        collidee.center_y.between?(center_y - @door_tolerance, center_y + @door_tolerance)
       end
       # puts "entering: #{entering}"
 
@@ -47,8 +47,8 @@ class Door < Zif::Sprite
         collidee.player_control = false # but this isn't a great way of doing it
         collidee.momentum.y = 0.0
         puts 'centering player'
-        collidee.x = center_x - (collidee.w * 0.5)
-        collidee.y = center_y - (collidee.h * 0.5)
+        collidee.x = center_x - (collidee.w.half)
+        collidee.y = center_y - (collidee.h.half)
       else
         play_once @sound_bounce
         bounce_off(collidee, facing)
