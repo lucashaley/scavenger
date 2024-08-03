f085924c0ed0451bcb910fe8bcd4abcc81173967
Lucas Haley <lucashaley@users.noreply.github.com>
2024-08-03 00:16:59 -0700

    Update faceable.rb

--- app/mixins/faceable.rb
+++ app/mixins/faceable.rb
@@ -1,31 +1,18 @@
 module Faceable
-  FACING = {
+  FACING_KEYED = {
     north:    0,
     east:     1,
     south:    2,
     west:     3,
   }.freeze
-  # FACING = [
-  #   :north,
-  #   :east,
-  #   :south,
-  #   :west
-  # ]
 
   attr_accessor :facing
 
-  # def face_turn_ccw
-  #   @facing = (@facing - 1) % 4
-  # end
-  # def face_turn_cw
-  #   @facing = (@facing + 1) % 4
-  # end
-
   # This is pretty ugly, but what the hell
   def face_turn_ccw
-    @facing = FACING.key((FACING[@facing] - 1) % 4)
+    @facing = FACING_KEYED.key((FACING_KEYED[@facing] - 1) % 4)
   end
   def face_turn_cw
-    @facing = FACING.key((FACING[@facing] + 1) % 4)
+    @facing = FACING_KEYED.key((FACING_KEYED[@facing] + 1) % 4)
   end
 end
